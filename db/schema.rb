f1b2824ac58f8ec210391a8770e738854778b389
nasia-sam <nasiasam@gmail.com>
2019-12-12 17:24:32 +0000

    Add notes and more

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_155340) do
+ActiveRecord::Schema.define(version: 2019_12_12_160402) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_155340) do
     t.integer "semester"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "notes", force: :cascade do |t|
