ff736fe740796a85e0126d035cdd7e6c6b91afb8
nasia-sam <nasiasam@gmail.com>
2019-12-14 19:51:38 +0000

    Upload files

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -69,6 +69,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:title, :content, :course_id)
+      params.require(:note).permit(:title, :content, :course_id, note_files: [])
     end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :course
+  has_many_attached :note_files
 end
