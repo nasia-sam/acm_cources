38fa9b9c833a7fbb978af238fc21bebc8094df76
nasia-sam <nasiasam@gmail.com>
2019-12-15 20:25:44 +0000

    Add validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
     has_many :notes
+    
+    validates :name, length: {minimum: 5}
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :course
   has_many_attached :note_files
+  
+  validates :title, presence: true
 end
